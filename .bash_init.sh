87b1d74beac8dc58954befb7ce194a0c370270de
DasBossGit <130844666+DasBossGit@users.noreply.github.com>
2024-06-20 16:40:36 +0200

    .

--- .bash_init.sh
+++ .bash_init.sh
@@ -4,7 +4,9 @@ is_local_accessable() {
     #fping -c1 -t100 -p0 https://kutt.mm-ger.com/cIrLl7
     curl --connect-timeout 1 --max-time 3 https://git.mm-ger.com/markus/bash_env/archive/main.tar.gz
 }
-
+get_hash() {
+    find /usr/share/bash_env -type f -print0 | sort -z | xargs -0 sha256sum | sha256sum
+}
 
 
 
--- .bash_setup.sh
+++ .bash_setup.sh
@@ -19,43 +19,82 @@ get_default_shell() {
 EOF
 
 #check if root
-if [ $(id -u) -ne 0 ]; then
-    echo "Needs to be run as root"
-    exit
-fi
+{
+    if [ $(id -u) -ne 0 ]; then
+        echo "Needs to be run as root" >&2
+        exit
+    fi
+}
 
 #check repositories
+{
+    if ! test -f /etc/apk/repositories; then
+        touch /etc/apk/repositories && chmod 777 /etc/apk/repositories
+    fi
+    repositories_file=$(cat /etc/apk/repositories)
+    repositories="""http://dl-cdn.alpinelinux.org/alpine/v3.19/main
+http://dl-cdn.alpinelinux.org/alpine/v3.19/community
+http://mirror.bahnhof.net/pub/alpinelinux/v3.19/main
+http://mirror.bahnhof.net/pub/alpinelinux/v3.19/community"""
+    if ! grep -Rq "#/media/cdrom/apks" <<<$repositories_file; then
+        echo "Fixing /etc/apk/repositories" >&2
+        echo """#/media/cdrom/apks
+$repositories""" >/etc/apk/repositories && echo "...Done"
+        repositories_file=$(cat /etc/apk/repositories)
+    fi
+    readarray -d "\n" -t repositories <<<$repositories
+    for repo in ${repositories[@]}; do
+        if ! grep -Rq "$repo" <<<$repositories_file; then
+            echo "Missing source in /etc/apk/repositories" >&2
+            echo "Adding \"$repo\"..."
+            echo "$repo" >>/etc/apk/repositories && echo "...Done"
+            repositories_file=$(cat /etc/apk/repositories)
+        fi
+    done
+    unset repositories_file
+    unset repositories
+    unset repo
+}
 
-#check fping
-if ! [ -x "$(command -v fping)" ]; then
-    echo "fping not found" >&2
-    echo "Installing..."
-    apk add fping >/dev/null && echo "fping installed successfully"
-fi
+#check repositories
+{
+    readarray -d $'\n' -t packages <<<$(echo """
+    fping%fping
+    curl%curl
+    neovim%nvim
+    python3%python3
+    """ | awk NF | awk '{$1=$1};1')
+    for package in ${packages[@]}; do
+        IFS=\% read -ra package <<<"$package"
+        if ! [ -x "$(command -v ${package[1]})" ]; then
+            echo "${package[0]} not found" >&2
+            echo "Installing..." #gets display before line above somehow
+            apk add "${package[0]}" >/dev/null && echo "${package[0]} installed successfully"
+        fi
+    done
+}
 
-#check curl
-if ! [ -x "$(command -v curl)" ]; then
-    echo "curl not found" >&2
-    echo "Installing..."
-    apk add curl >/dev/null && echo "curl installed successfully"
-fi
+#check bash_env folder
+{
+    if ! [ -d /usr ]; then
+        mkdir /usr
+    fi
+    if ! [ -d /usr/share ]; then
+        mkdir /usr/share
+    fi
+    if ! [ -d /usr/share/bash_env ]; then
+        mkdir /usr/share/bash_env && chmod 777 /usr/share/bash_env && echo "bash_env folder created..."
+    fi
+}
 
-#check nvim
-if ! [ -x "$(command -v nvim)" ]; then
-    echo "neovim not found" >&2
-    echo "Installing..."
-    apk add neovim >/dev/null && echo "neovim installed successfully"
-fi
-
-#check python
-if ! [ -x "$(command -v python3)" ]; then
-    echo "python3 not found" >&2
-    echo "Installing..."
-    apk add python3 >/dev/null && echo "python3 installed successfully"
-fi
-
-read -r -p "List users to setup (colon-separated): " users <<<"A;b"
+read -r -p "List users to setup (colon-separated): " users <<<"root;markus"
 readarray -d ";" -t users <<<$users
 for user in ${users[@]}; do
+    #if [ $user == "root" ]; then
     echo "$user"
+    #else
+    #    #just link to root
+    #    echo "$user"
+    #fi
+
 done
