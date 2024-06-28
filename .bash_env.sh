ca447c69a760b393111e963d242dd648241687da
DasBossGit <130844666+DasBossGit@users.noreply.github.com>
2024-06-28 15:41:55 +0200

    .

--- .bash_env.sh
+++ .bash_env.sh
@@ -6,7 +6,18 @@ export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 export NVIM="/usr/bin/nvim"
 
-export VIMRC="/usr/bin/nvim"
+wget -T 2 -q -O ~/.vimrc_git https://gist.githubusercontent.com/DasBossGit/29a0d68075f4c979adabc429e9172089/raw
+if [ -f ~/.vimrc ]; then
+
+    export VISUAL="$NVIM -u ~/.vimrc"
+    export VIMRC="~/.vimrc"
+elif [ -f ~/.vimrc ]; then
+    export VISUAL="$NVIM -u ~/.vimrc_git"
+    export VIMRC="~/.vimrc_git"
+else
+    export VISUAL="$NVIM"
+    export VIMRC=""
+fi
 
 export PS1="${a01a82e583105ab49dc3f83ba5fc24e5_PS1}"
 
